729ead9a7fb1324e78ac4163ac5005252300e7a0
st <st@A510PC3>
2026-04-08 18:38:37 +0300

    Task11 solved

--- Task11Project/logic.cpp
+++ Task11Project/logic.cpp
@@ -6,6 +6,22 @@
 
 #include "logic.h"
 
+double recursion(int vector[], int size) {
+	//base case
+	if (size == 1) {
+		return vector[0] > 0 ? 1 : 0;
+	}
+	//recursion case
+	size--;
+	return recursion(vector, size) + vector[size];
+}
+
 double calculate_average(int vector[], int size) {
-	return 0.0;
+
+	//passive fool-proof
+	if (size <= 0) {
+		return -1;
+	}
+
+	return recursion(vector, size) / size;
 }
